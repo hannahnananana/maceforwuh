0d4a66e0b6f11f39e45e9bef389dec40cb11e34a
gannah <hannahxoxobees@gmail.com>
2026-04-14 08:42:17 -0600

    Update MaceForWuh.java

--- src/main/java/com/wuh/maceforwuh/MaceForWuh.java
+++ src/main/java/com/wuh/maceforwuh/MaceForWuh.java
@@ -6,23 +6,26 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 public class MaceForWuh extends JavaPlugin implements CommandExecutor {
 
     @Override
     public void onEnable() {
-        getLogger().info(ChatColor.GOLD + "=== Wuh's Mace Plugin Enabled ===");
-        
-      
+        // Register the listener
         getServer().getPluginManager().registerEvents(new MaceListener(this), this);
         
-        // Register command
-        getCommand("wuhsmace").setExecutor(this);
+        // Register the command
+        if (getCommand("wuhsmace") != null) {
+            getCommand("wuhsmace").setExecutor(this);
+        }
         
-        getLogger().info(ChatColor.GOLD + "Wuh's Mace is ready!");
+        getLogger().info(ChatColor.GOLD + "Wuh's Mace is ready and loaded!");
     }
 
     @Override
@@ -39,11 +42,18 @@ public class MaceForWuh extends JavaPlugin implements CommandExecutor {
 
         Player player = (Player) sender;
 
+        // Check for permission (optional but recommended)
+        if (!player.hasPermission("wuhsmace.give")) {
+            player.sendMessage(ChatColor.RED + "You don't have permission to use this!");
+            return true;
+        }
+
         if (command.getName().equalsIgnoreCase("wuhsmace")) {
             ItemStack mace = createWuhsMace();
             player.getInventory().addItem(mace);
+            
             player.sendMessage(ChatColor.GOLD + "You received " + ChatColor.DARK_PURPLE + "Wuh's Mace" + ChatColor.GOLD + "!");
-            player.sendMessage(ChatColor.YELLOW + "Fall while holding it to break shields and get launched up 5 blocks!");
+            player.sendMessage(ChatColor.YELLOW + "Fall and hit a blocking player to shatter their shield!");
             return true;
         }
 
@@ -51,17 +61,30 @@ public class MaceForWuh extends JavaPlugin implements CommandExecutor {
     }
 
     public ItemStack createWuhsMace() {
+        // Using Diamond Hoe as the base
         ItemStack mace = new ItemStack(Material.DIAMOND_HOE);
         ItemMeta meta = mace.getItemMeta();
         
         if (meta != null) {
-            meta.setDisplayName(ChatColor.DARK_PURPLE + "Wuh's Mace");
-            meta.setLore(java.util.Arrays.asList(
-                ChatColor.YELLOW + "Use while falling to:",
-                ChatColor.YELLOW + "- Break shields in one hit",
-                ChatColor.YELLOW + "- Launch yourself up 5 blocks",
-                ChatColor.GRAY + "Cooldown: 30 seconds"
+            // Set the custom name
+            meta.setDisplayName(ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "Wuh's Mace");
+            
+            // Set the custom lore
+            meta.setLore(Arrays.asList(
+                "",
+                ChatColor.YELLOW + "Ability: " + ChatColor.WHITE + "Shield Breaker",
+                ChatColor.GRAY + "While falling, strike a blocking",
+                ChatColor.GRAY + "player to shatter their shield",
+                ChatColor.GRAY + "and launch yourself upward.",
+                "",
+                ChatColor.RED + "Cooldown: 30 Seconds"
             ));
+
+            // Make it look clean: Hide the fact that it's a Diamond Hoe
+            meta.setUnbreakable(true);
+            meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            meta.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+            
             mace.setItemMeta(meta);
         }
         
